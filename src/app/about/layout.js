ecb779da5bce31fd7a4f74b97a0dded9b98af957
Ayush_sccg <ayush1240945@gmail.com>
2023-09-29 16:54:17 +0545

    common layout

---
+++ src/app/about/layout.js
@@ -0,0 +1,10 @@
+export default function Layout({children}) {
+    return (
+        <div>
+            <h1>common layout for about page</h1>
+            {
+                children
+            }
+        </div>
+    )
+}
