81b09b65638696604d55f03e8ae2f4875bbb01de
Ayush_sccg <ayush1240945@gmail.com>
2023-10-02 15:31:14 +0545

    nothing

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 "use client";
 import Link from 'next/link'
-import styles from './page.module.css'
+import styles from './style.module.css'
 import {useRouter} from "next/navigation"
 
 export default function Home() {
