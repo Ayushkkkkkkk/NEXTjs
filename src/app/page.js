7b8cd2ebdc19971eb4bd71a38ab86da16c88bb56
Ayush_sccg <ayush1240945@gmail.com>
2023-10-02 15:23:01 +0545

    css/module

--- src/app/page.js
+++ src/app/page.js
@@ -10,8 +10,8 @@ export default function Home() {
   }
   return (
     <main className={styles.main}>
-      <h1>Routing</h1>
-      <h2>land</h2>
+      <h1 className='main'>Routing</h1>
+      <h2 className='main'>land</h2>
       <Link href="/login">Login</Link>
       <Link href="/about">About</Link>
 
