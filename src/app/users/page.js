43843fedf379c8976bf6c9420e3a17e4085a6f80
Ayush_sccg <ayush1240945@gmail.com>
2023-10-04 14:05:39 +0545

    static/site-generation

--- src/app/users/page.js
+++ src/app/users/page.js
@@ -6,10 +6,7 @@ export default async function Page(){
     console.log(land)
     return (
         <div>
-            <h1>User list</h1>
-            {
-               
-            }
+          
         </div>
     )
 }
