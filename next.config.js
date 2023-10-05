4fb701f4bb525d984b0fc8dafdb0022de3a1de0e
Ayush_sccg <ayush1240945@gmail.com>
2023-10-05 15:06:53 +0545

    rendering

--- next.config.js
+++ next.config.js
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    redirects:async()=>{
+        return [
+            {
+                source:'/user',
+                destination:'/',
+                permanent:false
+            },
+            {
+                source:'/user/:useridr',
+                destination:'/',
+                permanent:false
+            },
+            {
+                source:'/admin',
+                destination:'/',
+                permanent:false
+            }
+        ]
+    }
+}
 
 module.exports = nextConfig
--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -1,10 +1,11 @@
 import {redirect} from "next/navigation"
 
 export default function Page(){
+    redirect("/");
     return (
         
         <div>
-            <h1>User page</h1>
+            <h1>Admin page</h1>
             <h2>Heding 2 in user page</h2>
         </div>
     )
